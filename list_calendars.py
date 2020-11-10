06e6cbdc090f38d75a3a96252b0c446e477db94e
ckula <ckula@student.wethinkcode.co.za>
2020-11-10 16:20:31 +0200

    Added notes to fix errors

--- list_calendars.py
+++ list_calendars.py
@@ -16,5 +16,23 @@ def main():
        primary = "Primary" if calendar.get('primary') else ""
        print("%s\t%s\t%s" % (summary, id, primary))
 
+
+
+
+
+#  if err.resp.get('content-type', '').startswith('application/json'):
+#     reason = json.loads(err.content).get('error').get('errors')[0].get('reason')
+
+
+
+#  from googleapiclient.errors import HttpError
+#  try:
+#     ...
+#  except HttpError as err:
+#    # If the error is a rate limit or connection error,
+#    # wait and try again.
+#    if err.resp.status in [403, 500, 503]:
+#      time.sleep(5)
+#    else: raise
 if __name__ == '__main__':
    main()
