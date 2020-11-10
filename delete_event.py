a68d87300dbd758bb9f98640548adab12c3d615b
ckula <ckula@student.wethinkcode.co.za>
2020-11-10 16:14:10 +0200

    Initial commit

---
+++ delete_event.py
@@ -0,0 +1,20 @@
+## Script to delete events from google calendar
+from googleapiclient.discovery import build
+from cal_setup import get_calendar_service
+# from googleapiclient.errors import HttpError
+
+def main():
+    # Delete the event
+    service = get_calendar_service()
+    try:
+        service.events().delete(
+            calendarId='primary',
+            eventId='<place your event ID here>',
+        ).execute()
+    except googleapiclient.errors.HttpError:
+        print("Failed to delete event")
+
+    print("Event deleted")
+
+if __name__ == '__main__':
+    main()
